e09b9127303ef77728570c7fd84d6eae13745f58
natsu <natsu@natsus-iMac.local>
2019-01-08 23:36:30 -0800

    learning Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // locate custom html tag element and apply component to
+  // there are 2 options 
+  //1. template: '<h1>My App</h1>'  Note -  ES2015 we can use backtick `  ` for readablility
+  //2. templateUrl (allow us to inject html markup to template)
+  selector: 'app-root', 
   templateUrl: './app.component.html',
+  //styles or styleURLs property  works the same as template. it take an array []
   styleUrls: ['./app.component.css']
+  // styles: [`
+  // h1{ color: #ffffff }
+  // `,
+  //`
+  //.div {
+  //  font-style: italic;
+  //  color: green; 
+  // }
+  // `]
 })
 export class AppComponent {
-  title = 'basicAngular';
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { enableProdMode } from '@angular/core';
+
+//targeting browser. We need a bootstrap from browser platform, 
+//this function return a platform object that bootstrap module function on it
+//That is function you use to bootstrap module on platform
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+//root module
 import { AppModule } from './app/app.module';
+
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
+
+//make a call to the browser dynamic function, which would return an instance of the platform object
+//That object has method name bootstrapModule(), so we can call.
+//This function is expectin root module (which we already created "AppModule")
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
